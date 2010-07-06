a1ee8d19635451e0e26633174d906fcf77d04e92
Alex <zc00gii@gmail.com>
2010-07-06 11:48:54 -0400

    uglier, more functions

--- gl/zisp.gl.lisp
+++ gl/zisp.gl.lisp
@@ -3,9 +3,6 @@
 
 (in-package :cffi)
 
-(defvar *x* 0.525731112119133606)
-(defvar *z*  0.850650808352039932)
-
 (defun defcarray (array type)
   (declare (vector array))
   (let ((pointer (foreign-alloc type)))
@@ -17,6 +14,22 @@
     (values pointer)))
 
 (in-package :zisp.gl)
+
+(defparameter +x+ 0.525731112119133606)
+(defparameter +z+ 0.850650808352039932)
+(defparameter -+x+ (- +x+))
+(defparameter -+z+ (- +z+))
+
+(defparameter +vdata+
+  `#(#(,-+x+ 0.0 ,+z+) #(,+x+ 0.0 ,+z+) #(,-+x+ 0.0 ,-+z+) #(,+x+ 0.0 ,-+z+)
+    #(0.0 ,+z+ ,+x+) #(0.0 ,+z+ ,-+x+) #(0.0 ,-+z+ ,+x+) #(0.0 ,-+z+ ,-+x+)
+    #(,+z+ ,+x+ 0.0) #(,-+z+ ,+x+ 0.0) #(,+z+ ,-+x+ 0.0) #(,-+z+ ,-+x+ 0.0)))
+
+(defparameter +tindices+
+  #(#(0 4 1) #(0 9 4) #(9 5 4) #(4 5 8) #(4 8 1)
+    #(8 10 1) #(8 3 10) #(5 3 8) #(5 2 3) #(2 7 3)
+    #(7 10 3) #(7 6 10) #(7 11 6) #(11 0 6) #(0 1 6)
+    #(6 1 10) #(9 0 11) #(9 11 2) #(9 2 5) #(7 2 11)))
 
 (cffi:use-foreign-library %gl::opengl)
 
@@ -35,8 +48,10 @@
   (cffi::defcarray array type))
 
 (defun vertex-v4 (v)
-  (with-c-array (vertex v :float)
-    (%gl:vertex-4fv vertex)))
+  (apply #'gl:vertex (loop for i across v collect i)))
+
+;  (with-c-array (vertex v :float)
+;    (%gl:vertex-4fv vertex)))
 
 (defun vertex-v3 (v)
   (with-c-array (vertex v :float)
@@ -45,21 +60,16 @@
 (defun vertex-v2 (v)
   (with-c-array (vertex v :float)
     (%gl:vertex-2fv vertex)))
-#+ ()
-(defun vertex-pointer-3 (vertices)
-  (cffi:foreign-funcall "glDrawElements"
-			cl-opengl-bindings:int 3
-			cl-opengl-bindings:enum))
 
 (defun vertex (x &optional (y 0.0) (z 0.0) (w 0.0))
   (cond ((vectorp x)
-	 (cond ((= (length x) 4)
-		(vertex-v4 x)))
-	 (cond ((= (length x) 3)
-		(vertex-v3 x)))
-	 (cond ((= (length x) 2)
-		(vertex-v2 x))))
+	 (if (> (length x) 4) (error "Vector too long"))
+	 (apply #'gl:vertex (loop for i across x collect i)))
+	((listp x)
+	 (if (> (length x) 4) (error "List too long"))
+	 (apply #'gl:vertex x))
 	(t (gl:vertex x y z w))))
+
 (defun normal (x &optional y z)
   (cond ((vectorp x)
 	 (with-c-array (normal x :float)
@@ -67,22 +77,17 @@
 	(t (gl:normal x y z))))
 
 (defun normalize(v)
-  (declare ((vector * 3) v))
   (juggler::unit-vector v))
-;  (let ((d (sqrt (apply #'+
-;			(loop for i across v
-;			   collect (* i i))))))
-;    (apply #'vector (loop for i across v
-;				 collect (float (/ i d))))))
 
 (defun normalize!(v)
+  (declare ((vector * 3) v))
   (setf v (normalize v)))
 
 (defun normalize-cross-product (vector1 vector2)
   (normalize (juggler::cross-product vector1 vector2)))
 
 (defun subdivide (v1 v2 v3 depth index)
-  (declare ((array * 3) v1 v2 v3) (real depth index))
+  (declare ((vector * 3) v1 v2 v3) (real depth index))
   (let (v12 v23 v31)
     (if (zerop depth) t)
     (setf v12 (juggler::add-vector v1 v2)
@@ -98,7 +103,6 @@
     nil))
 
 (defun subdivide! (v1 v2 v3 depth index)
-  (declare ((array * 3) v1 v2 v3) (real depth index))
   (let (v12 v23 v31)
     (if (zerop depth) (render-triangle v1 v2 v3 index))
     (setf v12 (juggler::add-vector v1 v2)
@@ -212,13 +216,45 @@
 	     (gl:polygon-mode :front-and-back :fill)
 	     (render-cube 10 10 0 5 :quads)
 	     (gl:with-primitives :polygon
-	       (vertex-v #(-3.0 -3.0 0.0))
-	       (vertex-v #(-6.0 -3.0 0.0))
-	       (vertex-v #(-6.0 -6.0 0.0))
-	       (vertex-v #(-3.0 -6.0 0.0)))
+	       (vertex '(-3.0 -3.0 7.0))
+	       (vertex '(-6.0 -3.0 7.0))
+	       (vertex '(-6.0 -6.0 7.0))
+	       (vertex '(-3.0 -6.0 7.0)))
 	     (gl:polygon-mode :front-and-back :line)
 	     (render-cube 0 0 0 5 :quads)
 	     (gl:polygon-mode :front-and-back :point)
 	     (render-cube -10 -10 0 5 :quads)
+	     (gl:flush)
+	     (sdl:update-display))))))
+
+(defun 3d-test-2 ()
+  (let ((angle 0))
+  (sdl:with-init ()
+    (sdl:window 800 600
+		:title-caption "OpenGL Example"
+		:icon-caption "OpenGL Example"
+		:opengl t
+		:opengl-attributes '((:SDL-GL-DOUBLEBUFFER 1)))
+    (gl:viewport 0 0 800 600)
+    (gl:matrix-mode :projection)
+      (gl:load-identity)
+    (glu:perspective 45.0 (/ 800 600) 1.0 500.0)
+    (gl:matrix-mode :modelview)
+      (gl:load-identity)
+    (gl:clear-color 0 0 0 0)
+    (gl:translate 3 2 -60)
+    (sdl:with-events ()
+      (:quit-event () t)
+      (:idle ()
+	     (gl:clear :color-buffer-bit)
+	     (gl:color 1 1 1)
+	     (gl:rotate 1 1 1 1)
+	     (loop for i from 0 to 20 do
+		  (gl:enable :lighting)
+		  (gl:shade-model :smooth)
+		  (subdivide! (aref +vdata+ (aref (aref +tindices+ 0) i))
+			      (aref +vdata+ (aref (aref +tindices+ 1) i))
+			      (aref +vdata+ (aref (aref +tindices+ 2) i))
+			      0 i))
 	     (gl:flush)
 	     (sdl:update-display))))))
